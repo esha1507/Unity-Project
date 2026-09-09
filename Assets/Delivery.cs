d1c360443eb9b1565afa79d90f06b264ee259aec
esha1507 <eshadeshmukh07@gmail.com>
2026-09-09 19:38:52 +0530

    initial project upload

---
+++ Assets/Delivery.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage;
+    [SerializeField] float delay = 0.3f;
+     void OnTriggerEnter2D(Collider2D collision) 
+    {
+         if (collision.CompareTag("Package") && !hasPackage)
+        {
+             Debug.Log("Picked up package");
+             hasPackage = true;
+             GetComponent<ParticleSystem>().Play();
+             Destroy(collision.gameObject, delay);
+        }   
+
+         if (collision.CompareTag("Customer") && hasPackage)
+        {
+             Debug.Log("Deliverd Package");
+             hasPackage = false;
+             GetComponent<ParticleSystem>().Stop();
+            
+        }     
+
+    }
+}
---
+++ Assets/Fences/Readme.txt
@@ -0,0 +1,22 @@
+       --Stylised Fences Pack--
+______________________________________________
+  Thanks for downloading my asset pack!
+
+I'm still very new at this so if there
+is anything wrong or you have any 
+questions/ suggestions feel free to email
+me at e.j.cobden@gmail.com 
+
+______________________________________________
+
+Thank you again for supporting me, make sure
+to follow my socials for updates on any work
+im doing whether it be for this pack or 
+future packs/games.
+
+Socials:
+	Twitter:@elijah_cobden
+	Instagram:elijahcobden
+	Itch.io:ElijahCobden
+	
+
